3a9af92c7d5f6b07aa31d8da70b7fb830c1b2f13
vanesagambalara-eng <vanesagambalara@gmail.com>
2025-11-07 17:46:14 -0500

    Add files via upload

---
+++ paisCapital.py
@@ -0,0 +1,5 @@
+#pais y capital
+pais=input("escribe el pais")
+capital=input("escribe la caital")
+
+print (f"{capital} es la capital e {pais}")
---
+++ sumDos.py
@@ -0,0 +1,7 @@
+#suma de dos numeros
+num1=float(input("escribe el primer numero"))
+num2=float(input("escribe el segundo numero"))
+
+suma = num1+num2
+
+print ("El rsutado de la suma es" , suma)
